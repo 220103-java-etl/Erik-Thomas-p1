1e5542553919d8abe80e3d65f56bc5cca45711c1
Erik Thomas <yugioh_rocks@comcast.net>
2022-01-24 08:39:51 -0600

    actual code

---
+++ src/main/java/dev/thomas/models/User.java
@@ -0,0 +1,34 @@
+package dev.thomas.models;
+
+/**
+ * This concrete User class can include additional fields that can be used for
+ * extended functionality of the ERS application.
+ *
+ * Example fields:
+ * <ul>
+ *     <li>First Name</li>
+ *     <li>Last Name</li>
+ *     <li>Email</li>
+ *     <li>Phone Number</li>
+ *     <li>Address</li>
+ * </ul>
+ *
+ */
+public class User extends AbstractUser {
+
+    public User() {
+        super();
+    }
+
+    /**
+     * This includes the minimum parameters needed for the {@link dev.thomas.models.AbstractUser} class.
+     * If other fields are needed, please create additional constructors.
+     */
+    public User(int id, String username, String password, String role) {
+        super(id, username, password, role);
+    }
+
+    public User(int id, String firstName, String lastName, String username, String password, String role) {
+        super(id, firstName, lastName, username, password, role);
+    }
+}
