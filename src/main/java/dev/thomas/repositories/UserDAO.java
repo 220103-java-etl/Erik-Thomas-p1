2d17816346c945cc67bbd3bea724929f30e59bf2
RealmsEnd <83333157+RealmsEnd@users.noreply.github.com>
2022-01-24 14:47:34 -0600

    Revert "Delete src/main/java/dev/thomas directory"
    This reverts commit d2682fa8911996c928d43b42aab9c95cd85be201.

---
+++ src/main/java/dev/thomas/repositories/UserDAO.java
@@ -0,0 +1,80 @@
+package dev.thomas.repositories;
+
+import dev.thomas.models.User;
+import dev.thomas.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+
+    static ConnectionFactory cu = ConnectionFactory.getInstance();
+    /**
+     * Should retrieve a User from the DB with the corresponding username or an empty optional if there is no match.
+     */
+    public static User getByUsername(String username) {
+        String sql = "select * from users where username = ?";
+        try (Connection conn = cu.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                User u = new User(
+                        rs.getInt("id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("username"),
+                        rs.getString("passwrd"),
+                        rs.getString("role")
+                );
+                return u;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        String sql = "select * from users";
+        try(Connection conn = cu.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                User u = new User(
+                        rs.getInt("id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("username"),
+                        rs.getString("passwrd"),
+                        rs.getString("role")
+                );
+                users.add(u);
+            }
+            return users;
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * <ul>
+     *     <li>Should Insert a new User record into the DB with the provided information.</li>
+     *     <li>Should throw an exception if the creation is unsuccessful.</li>
+     *     <li>Should return a User object with an updated ID.</li>
+     * </ul>
+     *
+     * Note: The userToBeRegistered will have an id=0, and username and password will not be null.
+     * Additional fields may be null.
+     */
+    public User create(User userToBeRegistered) {
+        return userToBeRegistered;
+    }
+}
